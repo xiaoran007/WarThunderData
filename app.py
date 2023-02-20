c4937bd13b38c5e6d2e93cd9789b685c3ccd078f
xiaoran <xiaoran2495897824@163.com>
2023-02-20 20:19:20 +0800

    fix

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    return render_template('index.html')
+
+
+@app.route('/indev/')
+def indev():
+    return render_template('indev.html')
 
 
 if __name__ == '__main__':
